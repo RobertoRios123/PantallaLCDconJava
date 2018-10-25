c108c9f70d00e753f1db047d6d30beb501dfb1fe
RobertoRios123 <rirr_limon@outlook.com>
2018-10-24 22:21:19 -0500

    Add files via upload

---
+++ main.java
@@ -0,0 +1,11 @@
+package mensaje;
+
+public class main {
+
+    public static void main(String[] args) {        
+        vista_Mensaje vm = new vista_Mensaje();
+        vm.setVisible(true);
+        vm.setSize(400,300);
+	
+    }
+}
